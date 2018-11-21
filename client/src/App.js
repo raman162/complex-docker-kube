b70d9fcc9314c19b5fd136e7dd238b1c0aa006e6
Raman Walwyn-Venugopal <raman.walwyn@gmail.com>
2018-11-21 04:48:12 -0600

    changed header on page

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calculator</h1>
             <div>
               <Link to="/">Home</Link>
             </div>
